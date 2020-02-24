ceba77fc7aabf6ea12f2e63be09ce2c03625acf2
ArisQ <qiaoli.pg@gmail.com>
2020-02-24 15:53:13 +0800

    auto create rabbitmq queue

--- src/main/java/love/lipbcu/tacocloud/messaging/rabbitmq/RabbitMessagingConfig.java
+++ src/main/java/love/lipbcu/tacocloud/messaging/rabbitmq/RabbitMessagingConfig.java
@@ -1,5 +1,6 @@
 package love.lipbcu.tacocloud.messaging.rabbitmq;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +11,10 @@ public class RabbitMessagingConfig {
     @Bean
     public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue orderQueue() {
+        return new Queue("tacocloud.order.queue");
     }
 }
